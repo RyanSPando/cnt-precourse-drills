b7cf9821ddf3e5059077bdb4c3cdeb24469a4153
Ryan "archaen" Pando <vretha@gmail.com>
2017-02-02 10:02:11 -0700

    Address class complete.

---
+++ src/main/java/com/galvanize/Address.java
@@ -0,0 +1,52 @@
+package com.galvanize;
+
+public class Address {
+  private String street;
+  private String city;
+  private String state;
+  private String zip;
+
+  public Address(String street1, String city1, String state1, String zip1) {
+    street = street1;
+    city = city1;
+    state = state1;
+    zip = zip1;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public String getZip() {
+    return zip;
+  }
+
+  public void setStreet(String newStreet) {
+    street = newStreet;
+  }
+
+  public void setCity(String newCity) {
+    city =  newCity;
+  }
+
+  public void setState(String newState) {
+    state = newState;
+  }
+
+  public void setZip(String newZip) {
+    zip = newZip;
+  }
+
+  @Override public String toString() {
+    return street + ", " + city + ", " + state + " " + zip;
+  }
+
+}
